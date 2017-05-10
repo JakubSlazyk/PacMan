a7754537b949558074ccdb12009af6fed71fddd0
JakubSlazyk <slazyk.jakub@gmail.com>
2017-05-10 19:07:04 +0200

    Last test

--- src/Test.java
+++ src/Test.java
@@ -7,7 +7,7 @@ public class Test {
 		EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
-				System.out.print("TEST test teasfasfasf");
+				System.out.print("Najnowszy ostatni test");
 				new ObrazFrame();
 			}
 		});
